fd979686b6a4f44639fdd532b4235dbc48025b27
OnfireNFS <nathanf008@gmail.com>
2017-11-16 09:35:39 -0700

    Updated Shift To Use Electron
    Opposed to being a webpage. This allows for Node Integration

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const electron = require('electron')
-// Module to control application life.
+
 const app = electron.app
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
+  mainWindow = new BrowserWindow({
+    fullscreen: true,
+    kiosk: true,
+    backgroundColor: '#000000'
+  })
+  
+  mainWindow.setMenu(null);
+  
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
