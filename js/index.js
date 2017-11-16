83d3f3f19281c60900cd3b2edc5292eec6491832
OnfireNFS <nathanf008@gmail.com>
2017-11-16 13:00:14 -0700

    Added Read Consoles From Config.yml

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
+let consoles = [];
+
 $().ready(function() {
   $.post('config/config.yml')
     .done(function (data) {
     
       var config = jsyaml.load(data);
+      
+      $.each($(config).attr("consoles"), function(key, value)
+      {
+        consoles[key] = value.machine;
+        //Check if file exists here
+      });
+    
+    
+    //This prints out what the first console is and number of consoles registered in the system
+    
+      console.log(consoles[0]);
+      console.log(consoles.length);
     
       $.post('theme/' + $(config).attr("theme"))
         
