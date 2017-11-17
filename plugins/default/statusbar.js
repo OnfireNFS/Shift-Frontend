c61fc512f8ff9a94b03678151d936c6cccbfe5e3
OnfireNFS <nathanf008@gmail.com>
2017-11-17 13:55:11 -0700

    Added a digital clock to the statusbar

--- plugins/default/statusbar.js
+++ plugins/default/statusbar.js
@@ -11,14 +11,52 @@ $().ready(function() {
   statusbarConfig = $(config).attr("statusbar");
   
   $('head').append("<link rel='stylesheet' type='text/css' href='plugins/" + plugins[statusbarPluginID].file.replace(/\.[^/.]+$/, "") + ".css' />");
-  $(".content").html($(".content").html() + "<div class='statusbar'>12:00 PM</div>");
+  $(".content").html($(".content").html() + "<div class='statusbar'></div>");
   $(".statusbar").css("color", $(theme).attr("statusbar-font-color"));
   
   if(statusbarConfig.showConnection) {
     if(navigator.onLine) {
-      $(".statusbar").html("<i id='internet' class='icon ion-ios-world-outline'></i>" + $(".statusbar").html());
+      $(".statusbar").html("<i id='internet' class='" + $(statusbarConfig).attr("connectionIcon") + "'></i><p id='clock'>XX:XX</p>" + $(".statusbar").html());
     }
   }
   
+  statusbarClock();
+  window.setInterval(statusbarClock, 1000);
   
+  window.addEventListener("offline", function(){
+    $(".statusbar #internet").css("color", $(theme).attr("statusbar-font-deactivated"));
+  });
+  
+  window.addEventListener("online", function(){
+    $(".statusbar #internet").css("color", $(theme).attr("statusbar-font-color"));
+  });
 });
+
+function statusbarClock() {
+  var date = new Date();
+  var ampm = "";
+  var hours;
+  var minutes;
+  
+  if(!$(statusbarConfig).attr("24hourTime")) {
+    ampm = date.getHours() < 12
+             ? ' AM'
+             : ' PM';
+    
+    hours = date.getHours() == 0
+              ? 12
+              : date.getHours() > 12
+                ? date.getHours() - 12
+                : date.getHours();
+  }
+  
+  else {
+      hours = date.getHours();
+  }
+  
+  minutes = date.getMinutes() < 10 
+                ? '0' + date.getMinutes() 
+                : date.getMinutes();
+  
+  $(".statusbar #clock").html(hours + ":" + minutes + ampm);
+}
