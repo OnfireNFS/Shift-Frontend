36f030075728dee0f209717beea619f99481a046
OnfireNFS <nathanf008@gmail.com>
2017-11-17 18:18:30 -0700

    Moved Loading Into Functions

--- js/index.js
+++ js/index.js
@@ -3,57 +3,69 @@ let theme;
 let consoles = [];
 let plugins = [];
 let buttons = [];
+let buttonNames = [];
 
 $().ready(function() {
   $.post('config/config.yml')
     .done(function (data) {
     
-      //Load Config
       config = jsyaml.load(data);
     
-      //Load buttons and convert them to keycodes
-      $.each($(config).attr("buttons"), function(key, value) {
-        if(isNaN(value)){
-            buttons[key] = value.toUpperCase().charCodeAt(0);
-        }
-        
-        else {
-          buttons[key] = value;
-        }
-      });
+      loadButtons();
+      loadTheme();
+      loadPlugins();
+      loadConsoles();  
     
-      //Load theme
-      $.post('theme/' + $(config).attr("theme"))
-      .done(function (data) {
-        theme = jsyaml.load(data);
-
-        //Load fonts
-        $("head").prepend("<style type='text/css'> @font-face {font-family: 'Main'; src: url('resources/fonts/" + $(theme).attr("main-font") + "');}</style>");
-        
-        $(".background").css("background-color", $(theme).attr("background-color"));
-        $(".blackout").addClass("fadeOut");
-      })
-
-      .fail(function () {
-        alert("Could not load " + $(config).attr("theme"))
-      });
-    
-      //Load plugins
-      $.each($(config).attr("plugins"), function(key, value) {
-        plugins[key] = value.plugin;
-        $.getScript("plugins/" + plugins[key].file);
-      });
-    
-    
-      //Load consoles
-      $.each($(config).attr("consoles"), function(key, value)
-      {
-        consoles[key] = value.machine;
-        //Check if file exists here
-      });
     })
   
     .fail(function () {
       alert("fail");
     });
 });
+
+function loadButtons() {
+  $.each($(config).attr("buttons"), function(key, value) {
+    if(isNaN(value)){
+      buttons[key] = value.toUpperCase().charCodeAt(0);
+    }
+
+    else {
+      buttons[key] = value;
+    }
+  });
+  buttonNames = $(config).attr("buttonNames");
+}
+
+function loadConsoles() {
+  $.each($(config).attr("consoles"), function(key, value) {
+      consoles[key] = value.machine;
+      //Check if file exists here
+    });
+}
+
+function loadFonts() {
+  $("head").prepend("<style type='text/css'> @font-face {font-family: 'Main'; src: url('resources/fonts/" + $(theme).attr("main-font") + "');}</style>");
+}
+
+function loadPlugins() {
+  $.each($(config).attr("plugins"), function(key, value) {
+    plugins[key] = value.plugin;
+    $.getScript("plugins/" + plugins[key].file);
+  });
+}
+
+function loadTheme() {
+  $.post('theme/' + $(config).attr("theme"))
+  .done(function (data) {
+    theme = jsyaml.load(data);
+
+    loadFonts();
+
+    $(".background").css("background-color", $(theme).attr("background-color"));
+    $(".blackout").addClass("fadeOut");
+  })
+
+  .fail(function () {
+    alert("Could not load " + $(config).attr("theme"))
+  });
+}
